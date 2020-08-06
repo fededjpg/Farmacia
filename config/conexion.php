52a45bb1d1c455ea875bdf6130cbdec151c07dd8
fededjpg <fededj-247@hotmail.com>
2020-08-05 20:34:52 -0500

    modificado 100

--- config/conexion.php
+++ config/conexion.php
@@ -1,10 +1,11 @@
 <?php
 
-class Database{
-	public static function connect(){
-		$db = new mysqli('localhost', 'root', '', 'farmacia');
+class Database
+{
+	public static function connect()
+	{
+		$db = new mysqli('localhost', 'root', '22101998J@rr', 'farmacia');
 		$db->query("SET NAMES 'utf8'");
 		return $db;
 	}
 }
-
--- config/parameters.php
+++ config/parameters.php
@@ -1,7 +1,6 @@
 <?php
-define("base_url", "http://192.168.0.10/farmacia/");
+define("base_url", "http://64.227.14.1/farmacia");
 // define("base_url", "http://localhost/farmacia/");
 
 define("controller_default", "LoginController");
 define("action_default", "index");
-
